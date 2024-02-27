2e32d58d9d89d610a5e3fdeeba1e6adb6479edd5
Kot Alexey <leshakot10@gmail.com>
2024-02-27 20:48:56 +0300

    Исправлены следующие баги: 1)При вводе числа, превышающего верхнюю границу типа int и нажатии кнопки "Отгадать", происходил вылет приложения; 2)При вводе пустой строки и нажатии кнопки "Отгадать", происходил вылет приложения; Добавлены следующие функции: 1)При открытии приложения автоматически открывается клавиатура; 2)Клавиатура смещает вверх все элементы управления для того, чтобы клавиатура полностью вместилась.

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,49 +25,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println(comp_num);
+
         comp_num = GuessNum.rndCompNum();
         guessBtn = (Button) findViewById(R.id.guess_btn);
         attemptsLeftTxt = (TextView) findViewById(R.id.attempts_left_txt);
         hintShowTxt = (TextView) findViewById(R.id.hint_show_txt);
         enterNumber = (EditText) findViewById(R.id.num_user_txt);
+
+        enterNumber.requestFocus();
+        showSoftKeyboard(enterNumber);
+
         Context context = getApplicationContext();
+
         View.OnClickListener clckLstnr = new View.OnClickListener()
         {
             @Override
             public void onClick(View v) {
+                int attemptsLeft = Integer.parseInt(attemptsLeftTxt.getText().toString());
+                String textNumber = enterNumber.getText().toString();
+                if (textNumber.equals("")) return;
+
+                int number = Integer.parseInt(enterNumber.getText().toString());
+
+                if (attemptsLeft > 0)
                 {
-                    int attemptsLeft = Integer.parseInt(attemptsLeftTxt.getText().toString());
-                    int number = Integer.parseInt(enterNumber.getText().toString());
-
-                    if (attemptsLeft > 0)
+                    if (number == comp_num)
                     {
-                        if (number == comp_num)
-                        {
-                            guessBtn.setText(R.string.guessed_str);
-                            guessBtn.setClickable(false);
-                            Toast t = Toast.makeText(context, R.string.wishYou, Toast.LENGTH_LONG);
-                            t.show();
-                        }
-                        else if (number < comp_num)
-                        {
-                            hintShowTxt.setText(R.string.hint_more);
-                        }
-                        else if (number > comp_num)
-                        {
-                            hintShowTxt.setText(R.string.hint_less);
-                        }
-
-                        attemptsLeft--;
-                        attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
+                        guessBtn.setText(R.string.guessed_str);
+                        guessBtn.setClickable(false);
+                        showMessage(context, R.string.wishYou);
+                        return;
                     }
+                    else if (number < comp_num) hintShowTxt.setText(R.string.hint_more);
+                    else hintShowTxt.setText(R.string.hint_less);
 
-                    if (attemptsLeft == 0)
-                    {
-                        hintShowTxt.setText(R.string.defeat);
-                        Toast t = Toast.makeText(context, R.string.defeat, Toast.LENGTH_LONG);
-                        t.show();
-                    }
+                    attemptsLeft--;
+                    attemptsLeftTxt.setText(Integer.toString(attemptsLeft));
+                }
+
+                if (attemptsLeft == 0)
+                {
+                    hintShowTxt.setText(R.string.defeat);
+                    guessBtn.setClickable(false);
+                    showMessage(context, R.string.defeat);
                 }
             }
         };
@@ -80,5 +82,18 @@ public class MainActivity extends AppCompatActivity {
         hintShowTxt.setText(R.string.hint_show_str);
         guessBtn.setClickable(true);
         enterNumber.setText("");
+    }
+
+    public void showSoftKeyboard(View view) {
+        if (view.requestFocus()) {
+            InputMethodManager imm = getSystemService(InputMethodManager.class);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    public void showMessage(Context context, int message)
+    {
+        Toast t = Toast.makeText(context, message, Toast.LENGTH_LONG);
+        t.show();
     }
 }
